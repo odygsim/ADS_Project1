344e6fa9a2892c8874ced0d51d6069882081189d
odygsim <odygsim@gmail.com>
2019-10-15 15:22:25 +0300

    Correcting HQ_HT for difference in template class FunctionH definition . Correcting getting fi from point of type TID instead of Point*

--- inc/Hypercube_HT.h
+++ inc/Hypercube_HT.h
@@ -10,7 +10,7 @@
 #include "FunctionH.h"
 #include "util.h"
 
-template < class TD, class D, class TY, class Y>
+template<class TID>
 class Hypercube_HT {
 
 private:
@@ -18,13 +18,13 @@ private:
     int d, k, m, M, probes, k_hi;
     double w, r;
     // List of uniformly random hash functions h
-    std::list< FunctionH<TD,D,TY,Y> * > hList;
+    std::list< FunctionH<TID> * > hList;
     // Fi mapping : g to {0,1} uniformly
     std::unordered_map< int, int > fi_mapping;
     // Fi random pick
     int get_g_to_fi_mapping(int g);
     // G function calculation (by independent h functions)
-    int calculate_g(const Point * p);
+    int calculate_g(const TID &p);
     // Uniform random generator for mapping g to {0,1} : f
     std::mt19937 fgenerator;
     std::uniform_int_distribution<int> f_distr;
@@ -32,16 +32,14 @@ private:
 public:
     Hypercube_HT(double w, int d, int k, int m, int M, int probes, int k_hi, double r);
     ~Hypercube_HT();
-    // Getting the fi for given point - point class type
-    int get_fi(const Point *) ;
-    // Getting the fi for given point - data/label type
-    int get_fi(D ,Y);
+    // Getting the fi for given point
+    int get_fi(const TID &p) ;
 
 };
 
 // Hypercube HashTable constructor
-template < class TD, class D, class TY, class Y>
-Hypercube_HT<TD, D, TY, Y>::Hypercube_HT(double w = 4, int d, int k = 3, int m = 2^32-5, int M, int probes = 2, int k_hi = 4, double r){
+template<class TID>
+Hypercube_HT<TID>::Hypercube_HT(double w = 4, int d, int k = 3, int m = 2^32-5, int M, int probes = 2, int k_hi = 4, double r=0){
 
     // Assign parameters
     this->d = d;
@@ -54,7 +52,7 @@ Hypercube_HT<TD, D, TY, Y>::Hypercube_HT(double w = 4, int d, int k = 3, int m =
 
     // Get h new functions
     for ( int i=1 ; i <= k_hi ; i++) {
-        hList.push_back(new FunctionH<TD, D, TY, Y> (4*r, d, k) );
+        hList.push_back(new FunctionH<TID> (4*r, d, k) );
     }
 
     // Create a uniform random generator for g to f {0,1} values
@@ -69,12 +67,12 @@ Hypercube_HT<TD, D, TY, Y>::Hypercube_HT(double w = 4, int d, int k = 3, int m =
 }
 
 // Hypercube HashTable destructor
-template < class TD, class D, class TY, class Y>
-Hypercube_HT<TD, D, TY, Y>::~Hypercube_HT(){
+template<class TID>
+Hypercube_HT<TID>::~Hypercube_HT(){
 
     // Get hList start and end positions
-    typename std::list <FunctionH<TD,D,TY,Y> *>::iterator hListStart = hList.begin();
-    typename std::list <FunctionH<TD,D,TY,Y> *>::iterator hListEnd = hList.end();
+    typename std::list <FunctionH<TID> *>::iterator hListStart = hList.begin();
+    typename std::list <FunctionH<TID> *>::iterator hListEnd = hList.end();
     // Erase all elements in hList
     hList.erase(hListStart, hListEnd);
 
@@ -82,63 +80,54 @@ Hypercube_HT<TD, D, TY, Y>::~Hypercube_HT(){
 
 
 // Get fi mapping for given g
-template < class TD, class D, class TY, class Y>
-int Hypercube_HT<TD, D, TY, Y>::get_g_to_fi_mapping(int g){
+template<class TID>
+int Hypercube_HT<TID>::get_g_to_fi_mapping(int g){
 
+    int fi = 0;
     // Get existing g->f mapping or create a new one
     if ( fi_mapping.find(g) == fi_mapping.end() ) {
-        // create new (f, g) pair
-        int newFmapping = f_distr(fgenerator);
-        fi_mapping.insert( std::make_pair (g, newFmapping) );
-        return newFmapping;
+        // create new (f, g) pair - add to mapping
+        fi = f_distr(fgenerator);
+        fi_mapping.insert( std::make_pair (g, fi) );
     } else {
         // existing (f, g) pair
-        int fmapping = fi_mapping.at(g);
-        return fmapping;
+        fi = fi_mapping.at(g);
     }
+
+    return fi;
 }
 
 // Galculate g function based on given point p and hi's
-template < class TD, class D, class TY, class Y>
-int Hypercube_HT<TD, D, TY, Y>::calculate_g(const Point *p){
+template<class TID>
+int Hypercube_HT<TID>::calculate_g(const TID &p){
 
-    // Calculate the g from the k Hi
+    // Calculate g from the k Hi
     std::vector<int> hValues;
-    int g = 0;
+    int g;
     //Calculate the values of each h
-    for(auto h: hList){
+    for(FunctionH<TID> h: hList){
         hValues.push_back(h->calculatePoint(p));
     }
-    // Get g from concat of hi's integer numbers
-    std::string gstr;
-    for (int i = 0; i < k; ++i) {
-        gstr += std::to_string(hValues[i]); //concatenate all hi' s
+    // Get g from concat of hi's integer numbers bitwise
+    g = hValues[0];
+    for (int i = 1; i < k_hi; ++i) {
+        g = (g << k_hi) | hValues[i]; //concatenate the k bits of all Hi
     }
 
-    // Convert concatenated string to integer
-    int g = std::stoi(gstr);
     return g;
 }
 
 // Get final fi for given point - case P is Point
-template < class TD, class D, class TY, class Y>
-int Hypercube_HT<TD, D, TY, Y>::get_fi(const Point * p) {
+template<class TID>
+int Hypercube_HT<TID>::get_fi(const TID &p) {
     int g;
 
     // Calculate g function for point p
     int g = calculate_g(p);
     // Get associate fi mapping to g
-    int fi = get_g_to_fi_mapping();
+    int fi = get_g_to_fi_mapping(g);
     // return fi
     return fi;
-}
-
-// Get final fi for given point - case P is in "Data", "Label" format
-template < class TD, class D, class TY, class Y>
-int Hypercube_HT<TD, D, TY, Y>::get_fi(D data, Y label) {
-
-    // Todo : code to be completed for point data provided in this format
-    return 0;
 }
 
 #endif //ADS_PROJECT1_HYPERCUBE_HT_H
